ee64634a753f17cc58247a7503ad70570f62b9bb
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-10 11:03:24 +0700

    search bar issue..

--- app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
+++ app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
@@ -1,13 +1,43 @@
 package id.co.blogspot.tutor93.mymaps;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_notifications:
+                    return true;
+                case R.id.navigation_favorite:
+                    return true;
+                case R.id.navigation_profile:
+                    return true;
+            }
+
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     }
+
 }
