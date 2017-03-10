8884462083df9295711d0d6c5f2e94c95c5922ce
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-11 04:37:10 +0700

    bad action bar..

--- app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
+++ app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
@@ -1,19 +1,18 @@
 package id.co.blogspot.tutor93.mymaps;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.SearchView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -31,10 +30,36 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private ImageView searchButton_toolbar;
+    private EditText textSearch;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        textSearch = (EditText) findViewById(R.id.textSearch);
+        searchButton_toolbar = (ImageView) findViewById(R.id.search_btn);
+        toolbar = (Toolbar) findViewById(R.id.toolbar3);
+
+
+
+        searchButton_toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!textSearch.isShown()){
+                    textSearch.setVisibility(View.VISIBLE);
+                }else {
+                    textSearch.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
