c8fffc9cf3365e69830e90c2d64d8b9b6d48db8a
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-11 07:12:44 +0700

    success, change map style

--- app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
+++ app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
@@ -2,14 +2,9 @@ package id.co.blogspot.tutor93.mymaps;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -46,9 +41,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     };
 
-    private ImageView searchButton_toolbar;
-    private EditText textSearch;
-    private Toolbar toolbar;
     private GoogleMap m_map;
     private boolean mapReady;
 
@@ -57,33 +49,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
-            actionBar.setHomeButtonEnabled(true);
-        }
-
-        textSearch = (EditText) findViewById(R.id.textSearch);
-        searchButton_toolbar = (ImageView) findViewById(R.id.search_btn);
-        toolbar = (Toolbar) findViewById(R.id.toolbar3);
-
-
-
-        searchButton_toolbar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!textSearch.isShown()){
-                    textSearch.setVisibility(View.VISIBLE);
-                }else {
-                    textSearch.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
 
         SupportMapFragment mapFragment = (SupportMapFragment)
-                getSupportFragmentManager().findFragmentById(R.id.fragment);
+                getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
     }
 
@@ -91,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mapReady = true;
         m_map = googleMap;
-//        LatLng mCibodas = new LatLng(-7.011146, 107.764331); //cibodas
+//      LatLng mCibodas = new LatLng(-7.011146, 107.764331); //cibodas
         LatLng mNew_York = new LatLng(40.7484, -73.9857); //new york
         CameraPosition target = CameraPosition.builder().target(mNew_York).zoom(14).build();
         m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
