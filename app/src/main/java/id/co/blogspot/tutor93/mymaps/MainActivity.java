550933fe90c7eb3050819d86de88dfbe112cb67b
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-11 14:24:26 +0700

    adding marker onclick

--- app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
+++ app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
@@ -12,6 +12,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
@@ -51,6 +53,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private LatLng mNew_York = new LatLng(40.7484, -73.9857); //new york
     private LatLng mCibodas = new LatLng(-7.011146, 107.764331); //cibodas
     private static final int ANIMATE_TIME = 10000; //10 seconds
+    private MarkerOptions cibodasPosition, newYorkPosition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +63,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        cibodasPosition = new MarkerOptions()
+                .position(mCibodas)
+                .title("CIBODAS");
+
+        newYorkPosition = new MarkerOptions()
+                .position(mNew_York)
+                .title("NEW YORK");
+
 
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.map_fragment);
@@ -69,7 +82,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mapReady = true;
         m_map = googleMap;
-
+        m_map.addMarker(newYorkPosition);
+        m_map.addMarker(cibodasPosition);
+        
+        //on marker click...
+        m_map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                moveCamera(marker.getPosition(), true);
+                return true;
+            }
+        });
     }
 
     private void moveCamera(LatLng latLng, boolean isAnimate){
