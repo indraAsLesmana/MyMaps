3f7dbf8cd02c77dbd8daef5cc93cbf199c342125
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-13 07:02:58 +0700

    toggle menu animation work

--- app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
+++ app/src/main/java/id/co/blogspot/tutor93/mymaps/MainActivity.java
@@ -1,13 +1,21 @@
 package id.co.blogspot.tutor93.mymaps;
 
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -73,10 +81,27 @@ public class MainActivity extends AppCompatActivity implements
     private MarkerOptions cibodasPosition, newYorkPosition;
     private Marker markerCibodas;
 
+    private Toolbar toolbar;
+    private DrawerLayout navDrawer;
+    private NavigationView navView;
+    private ActionBarDrawerToggle toggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null ){
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            setUpNavDrawer();
+        }
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
@@ -174,5 +199,78 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onMapLongClick(LatLng latLng) {
 
+    }
+
+    private void setUpNavDrawer() {
+        navDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                navDrawer.closeDrawers();
+                onNavigationItemClick(item.getItemId());
+                return true;
+            }
+        });
+
+        toggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                navDrawer,         /* DrawerLayout object */
+                toolbar,  /* nav drawer icon to replace 'Up' caret */
+                R.string.open,  /* "open drawer" description */
+                R.string.close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View drawer) {
+                super.onDrawerClosed(drawer);
+                invalidateOptionsMenu();
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawer) {
+                super.onDrawerOpened(drawer);
+                invalidateOptionsMenu();
+            }
+        };
+        navDrawer.addDrawerListener(toggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onNavigationItemClick(int id) {
+        /*switch (id) {
+            case R.id.nav_item_dash:
+                title.setText("Dashboard Activity");
+                break;
+            case R.id.nav_item_cal:
+                title.setText("Calendar Activity");
+                break;
+            case R.id.nav_item_prefs:
+                title.setText("Preference Activity");
+                break;
+        }*/
     }
 }
